f0c1e9951ce0a80d39abb1063d3de7db80417605
nukari-r <82310051@sankogakuen.jp>
2023-11-22 14:43:09 +0900

    制限時間と邪魔ブロック追加

---
+++ Assets/Scripts/RollABall/InGame/PendulumBlock.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PendulumBlock : MonoBehaviour
+{
+    public float Amplitude = 5f;
+    public float Frequency = 1f;
+    private Vector3 startPos = Vector3.zero;
+
+    private MeshRenderer boxMesh;
+    float time = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        boxMesh = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = startPos + Vector3.forward * Amplitude * Mathf.Sin(Time.time * Frequency);
+
+        time -= Time.deltaTime;
+        if (time < 0f)
+        {
+            boxMesh.enabled = !boxMesh.enabled;
+            time = 1f;
+        }
+    }
+}
